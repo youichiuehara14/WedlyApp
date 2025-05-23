cba8e278e1fb1793a9e8aa3c1e4c50107a48e755
Youichi <46785068+youichiuehara14@users.noreply.github.com>
2025-05-24 04:31:04 +0800

    add credential: ture to new server instance

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,7 @@ const io = new Server(server, {
   cors: {
     origin: ['http://localhost:5173', 'https://wedlyapp.onrender.com'],
     methods: ['GET', 'POST'],
+    credentials: true,
   },
 });
 const PORT = process.env.PORT || 4000;
